e45a8bd87631304c22693a477be9afa9173c71ec
Alejandr0112 <erickalejandro0112@gmail.com>
2025-02-14 14:13:02 -0600

    Tablas de multiplicar 4 y 6

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Tabla de multiplicar del 4:" << endl;
+    for (int i = 1; i <= 10; i++) {
+        cout << "4 x " << i << " = " << (4 * i) << endl;
+    }
+
+    cout << "\nTabla de multiplicar del 6:" << endl;
+    for (int i = 1; i <= 10; i++) {
+        cout << "6 x " << i << " = " << (6 * i) << endl;
+    }
+
+    return 0;
+}
+
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
